9ab9c5fa7c75d354109c965625e2c9c73220158e
Edgar-ae <edgar.aggre@gmail.com>
2024-06-14 20:33:06 -0500

    [feat][yo] TaskSprint #3

--- app/controllers/home_controller.py
+++ app/controllers/home_controller.py
@@ -2,16 +2,21 @@ import tkinter
 import tkinter as tk
 from tkinter import filedialog
 import os
-# home_controller.py
+from tkinter import messagebox
+
 class HomeController:
     def __init__(self, view):
         self.view = view
-        self.view.boton_send.config(command=self.update_label)
+        self.list_files = []
+        self.new_list_files = []
+        self.view.boton_rename.config(command=self.renamer_files)
         self.view.boton_select.config(command=self.selected_directory)
+        self.view.boton_clear.config(command=self.clear_list)
         self.init_views()
 
     def limit_entry(self, *args):
         value = self.var_entrada_extencion.get()
+        self.chanel_extencion(value[:4])
         if len(value) > 4:  # Limitar a 4 caracteres
             self.var_entrada_extencion.set(value[:4])
 
@@ -24,11 +29,36 @@ class HomeController:
         self.var_entrada_extencion.trace_add('write', self.limit_entry)
         self.view.entrada_extencion.config(textvariable=self.var_entrada_extencion)
 
-    def update_label(self):
-        # name = self.view.entry.get()
-        # self.view.result_label.config(text=f"Hello, {name}!")
-        # print("Acurazao")
-        print("Acurazao")
+        self.view.seleccion_renameType.config(values=["Renombrar", "Sustituir"])
+        self.view.seleccion_renameType.set("Renombrar")
+        self.view.seleccion_renameType.bind('<<ComboboxSelected>>', self.renamer_type)
+
+        self.valor_entry = tkinter.StringVar() # ESTA ES UNA VARIABLE DE CONTROL 'valor_entry'
+        self.valor_entry.trace_add('write', self.view_change_name)
+        self.view.entrada_newName.config(textvariable=self.valor_entry)
+
+        self.valor_SustiName = tkinter.StringVar()
+        self.valor_SustiName.trace_add('write', self.Sustit_Name)
+        self.view.entrada_SustiName.config(textvariable=self.valor_SustiName)
+
+        
+        self.valor_SustiName2 = tkinter.StringVar()
+        self.valor_SustiName2.trace_add('write', self.Sustit_Name)
+        self.view.entrada_SustiName2.config(textvariable=self.valor_SustiName2)
+
+    def renamer_files(self):
+        i = 0
+        for n in self.list_files:
+            if os.path.exists(f'{self.directorio}/{self.new_list_files[i]}'):
+                if self.view.seleccion_renameType.get() == "Renombrar":
+                    messagebox.showerror("Error, conflicto", f'Ya existe un archivo con este nombre: {self.new_list_files[i]}, procura que los nombres sean diferentes!')
+                    break
+            else:
+                os.rename(f'{self.directorio}/{n}', f'{self.directorio}/{self.new_list_files[i]}')
+            i += 1
+        messagebox.showinfo("Messaje", "Renombramiento Finalizado!")
+        self.reset_values()
+        self.listar_archivos()
 
     def on_combobox_select(self, event):
         if self.view.seleccion.get() == "Incluir Extensión":
@@ -36,21 +66,85 @@ class HomeController:
         else:
             self.view.entrada_extencion.pack(padx=10, pady=10)
 
-    def listar_archivos(self, directorio):
+    def listar_archivos(self):
         # Obtener una lista de todos los archivos en el directorio
-        archivos = os.listdir(directorio)
+        archivos = os.listdir(self.directorio)
 
         # Imprimir la lista de archivos
         i = 0
+        self.list_files = []
+        self.view.lista_files.delete(0, tkinter.END) # LIMPIAR LISBOX
         for archivo in archivos:
             i += 1
             self.view.lista_files.insert(tkinter.END, archivo)
-            print(archivo)
+            self.list_files.append(archivo)
         self.view.lista_files.configure(height=i)
 
     def selected_directory(self):
-        directorio = filedialog.askdirectory(initialdir="/home/jeiro/_TALLER-L_/")
-        print(directorio)
-        self.listar_archivos(directorio)
+        self.directorio = filedialog.askdirectory(initialdir="/home/jeiro/_TALLER-L_/")
+        self.listar_archivos()
 
+    def view_change_name(self, *args):
+        value = self.valor_entry.get()
+        i = 0
+        self.new_list_files = []
+        for n in self.list_files:
+            i += 1
+            self.view.lista_files.delete(0)
+            extension = n.split('.')[-1]
+            output_num = value.replace('{num}', f'{i:02d}')
+            self.view.lista_files.insert(tkinter.END, f'{output_num}.{extension}')
+            self.new_list_files.append(f'{output_num}.{extension}')
     
+    def renamer_type(self, event):
+        if self.view.seleccion_renameType.get() == "Renombrar":
+            print("Renombrar")
+            self.view.entrada_newName.grid(row=0, column=1, padx=10, pady=10)
+            self.view.entrada_SustiName.grid_forget()
+            self.view.etiqueta_señal.grid_forget()
+            self.view.entrada_SustiName2.grid_forget()
+        else:
+            print("Sustituir")
+            self.view.entrada_newName.grid_forget()
+            self.view.entrada_SustiName.grid(row=0, column=0, padx=10, pady=10)
+            self.view.etiqueta_señal.grid(row=0, column=1)
+            self.view.entrada_SustiName2.grid(row=0, column=2, padx=10, pady=10)
+
+    def Sustit_Name(self, *args):
+        value = self.valor_SustiName.get()
+        value2 = self.valor_SustiName2.get()
+
+        self.new_list_files = []
+        i = 0
+        for n in self.list_files:
+            i += 1
+            self.view.lista_files.delete(0)
+            final_rename = n.replace(value, value2)
+            output_num = final_rename.replace('{num}', f'{i:02d}')
+            self.view.lista_files.insert(tkinter.END, output_num)
+            self.new_list_files.append(output_num)
+
+    def clear_list(self):
+        self.view.lista_files.delete(0, tkinter.END)
+        self.list_files = []
+        self.view.lista_files.configure(height=0)
+
+    def chanel_extencion(self, value):
+        other_list = []
+        if self.new_list_files:
+            other_list = self.new_list_files
+        else:
+            other_list = self.list_files
+        self.new_list_files = []
+        for n in other_list:
+            self.view.lista_files.delete(0)
+            extension = n.split('.')[-1]
+            extension_modify = n.replace(extension, value)
+            self.view.lista_files.insert(tkinter.END, extension_modify)
+            self.new_list_files.append(extension_modify)
+
+    def reset_values(self):
+        self.view.entrada_newName.delete(0, tkinter.END)
+        self.view.entrada_SustiName.delete(0, tkinter.END)
+        self.view.entrada_SustiName2.delete(0, tkinter.END)
+        self.view.entrada_extencion.delete(0, tkinter.END)
--- app/views/home_view.py
+++ app/views/home_view.py
@@ -23,33 +23,41 @@ class HomeView(tk.Frame):
         etiqueta = tkinter.Label(frame_extension, text="Extensión", bg="orange", fg="white", font=("Helvetica", 10, "bold")) # Crea una 
         etiqueta.pack(padx=10, pady=10) # Margin en x,y
         self.seleccion = ttk.Combobox(frame_extension)
-        self.seleccion.set("Incluir Extensión")
         self.seleccion.pack()
 
-        self.entrada_extencion = tkinter.Entry(frame_extension, textvariable='patata')
+        self.entrada_extencion = tkinter.Entry(frame_extension)
         self.entrada_extencion.pack_forget()
 
-        # ANCHOR ENTRADA DE TEXTO
-        etiqueta = tkinter.Label(frame1, text="Las variables para la identificacion \n del los archivos renombrados son {num} o {letter}", bg="red", fg="white", font=("Helvetica", 10, "bold")) # Crea una 
-        etiqueta.pack(padx=10, pady=10) # ALGO SIMILAR A MARGIN
-        # Entrada
-        # valor_entry = "hola mundo"
-        
-        self.valor_entry = tkinter.StringVar() # ESTA ES UNA VARIABLE DE CONTROL 'valor_entry'
-        self.valor_entry.trace_add('write', self.limit_entry2)
-        self.entrada_newName = tkinter.Entry(frame1, textvariable=self.valor_entry)
-        # self.entrada_newName.bind('<Key>', self.limit_entry2)
-        self.entrada_newName.pack(padx=10, pady=10)
+        etiqueta = tkinter.Label(frame1, text="La variable de numeración es: {num}", bg="red", fg="white", font=("Helvetica", 10, "bold")) # Crea una 
+        etiqueta.pack(padx=10, pady=10) # Margin en x,y
 
-        # ANCHOR BUTTON
-        self.boton_send = tkinter.Button(frame1, text="Renombrar")
-        self.boton_send.pack(padx=10, pady=10)
-
-    def limit_entry2(self, *args):
-        print("hhh")
-        # value = self.valor_entry.get()
-        # print(value)
+        self.seleccion_renameType = ttk.Combobox(frame1)
+        self.seleccion_renameType.pack(padx=10, pady=10)
         
+        frame_nameInput = tkinter.Frame(frame1, bg="green", width=100)
+        frame_nameInput.pack(fill=tkinter.X)
+        frame_nameInput.columnconfigure(0, weight=1)
+        frame_nameInput.columnconfigure(1, weight=1)
+        frame_nameInput.columnconfigure(2, weight=1)
+
+        self.entrada_newName = tkinter.Entry(frame_nameInput)
+        self.entrada_newName.grid(row=0, column=1, padx=10, pady=10)
+
+        self.entrada_SustiName = tkinter.Entry(frame_nameInput)
+        self.entrada_SustiName.grid(row=0, column=0, padx=10, pady=10)
+        self.entrada_SustiName.grid_forget()
+
+        self.etiqueta_señal = tkinter.Label(frame_nameInput, text="-->", bg="green", fg="black", font=("Helvetica", 10, "bold"))
+        self.etiqueta_señal.grid(row=0, column=1)
+        self.etiqueta_señal.grid_forget()
+
+        self.entrada_SustiName2 = tkinter.Entry(frame_nameInput)
+        self.entrada_SustiName2.grid(row=0, column=2, padx=10, pady=10)
+        self.entrada_SustiName2.grid_forget()
+
+        self.boton_rename = tkinter.Button(frame1, text="Renombrar")
+        self.boton_rename.pack(padx=10, pady=10)
+
     def files_list(self):
         frame2 = tkinter.Frame(self, bg="blue", width=200)
         frame2.pack(side=tkinter.LEFT, fill=tkinter.BOTH, expand=True)
@@ -59,6 +67,8 @@ class HomeView(tk.Frame):
         self.lista_files = tkinter.Listbox(frame2, height=0)
         self.lista_files.pack()
 
-        # ANCHOR BUTTON
         self.boton_select = tkinter.Button(frame2, text="Seleccionar Carpeta")
         self.boton_select.pack(padx=10, pady=10)
+
+        self.boton_clear = tkinter.Button(frame2, text="Limpiar Lista")
+        self.boton_clear.pack(padx=10)
